4d62168a55629e3066e0b5cbd9f26d455ff0195e
D-E-N-Y <mysliaev.daniil@gmail.com>
2025-08-14 16:05:42 +0300

    add finish panel

--- Assets/Scripts/Systems/Game/Bootstraper/GameBootstraper.cs
+++ Assets/Scripts/Systems/Game/Bootstraper/GameBootstraper.cs
@@ -11,12 +11,16 @@ public class GameBootstraper : MonoBehaviour
 
     private void Start()
     {
+        Time.timeScale = 1f;
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 120;
 
         player.SetControlers(gameUICanvas.GetUIFixedJoystick(), gameUICanvas.GetMelleAttackButton());
         player.Initialize();
+
         cameraOrigin.Initialize(player.transform);
+
+        gameUICanvas.Initialize(player);
 
         waveSystem.Initialize(player);
         waveSystem.StartWave();
--- Assets/Scripts/UI/Systems/Game/Canvas/GameUICanvas.cs
+++ Assets/Scripts/UI/Systems/Game/Canvas/GameUICanvas.cs
@@ -6,6 +6,13 @@ public class GameUICanvas : MonoBehaviour
     [SerializeField] private FixedJoystick uiJoystick;
     [SerializeField] private Button melleAttackButton;
 
+    [SerializeField] private UI_FinishGamePanel ui_finishGamePanel;
+
+    public void Initialize(Player player)
+    {
+        ui_finishGamePanel.Initialize(player);
+    }
+
     public FixedJoystick GetUIFixedJoystick() => uiJoystick;
     public Button GetMelleAttackButton() => melleAttackButton;
 }
